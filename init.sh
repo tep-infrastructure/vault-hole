f4b8ab9a3ce13843b99e33478d05c7dab4b60277
Thomas Holmes <thomasholmes@outlook.com>
2021-02-25 00:34:52 +0000

    Added missing apt update.

--- init.sh
+++ init.sh
@@ -23,6 +23,7 @@ sudo add-apt-repository \
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io vault git vim && \
 sudo systemctl enable docker && sudo systemctl start docker
 
